eef1b64cf5239e1b94872a59153cf23cdf8af443
DoctorDenis <sychovdenis@icloud.com>
2022-11-30 22:53:24 +0200

    Changed server.js file. Added link to mongodb

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const { app } = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const { DB_URI, PORT } = process.env;
+const DB_URI =
+  "mongodb+srv://denis:Ot0UzVGiGGwOpjsC@wallet-database.q8jesgd.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = 3000;
 
 mongoose
   .connect(DB_URI)
