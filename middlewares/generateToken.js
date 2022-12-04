231a48247276bd0ea6c97e883b91272dfd055c02
Константин Олейник <oks@sto.local>
2022-12-04 14:16:18 +0200

    fix bugs swagger

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -1,16 +1,17 @@
-// require("dotenv").config();
-const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 // ACCES_TOKEN_SEKRET_KEY=wi".A3h2Bu3_j+`
 // REFRESH_TOKEN_SEKRET_KEY=!uA=^B_aom:Wx7v
 
-
-
 const { ACCES_TOKEN_SEKRET_KEY, REFRESH_TOKEN_SEKRET_KEY } = process.env;
 
 module.exports = function generateToken(payload) {
-  const accesToken = jwt.sign(JSON.stringify(payload), ACCES_TOKEN_SEKRET_KEY );
-  const refreshToken = jwt.sign(JSON.stringify(payload), REFRESH_TOKEN_SEKRET_KEY);
+  const accesToken = jwt.sign(JSON.stringify(payload), ACCES_TOKEN_SEKRET_KEY);
+  const refreshToken = jwt.sign(
+    JSON.stringify(payload),
+    REFRESH_TOKEN_SEKRET_KEY
+  );
   return {
     accesToken,
     refreshToken,
