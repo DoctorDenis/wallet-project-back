b7676c9c23f1eda36ef8ea836380d94730f8c3fe
Константин Олейник <oks@sto.local>
2022-12-10 11:23:36 +0200

    add route users/current

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,5 +2,6 @@ const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
 const refresh = require("./refresh");
+const getCurrent = require("./getCurrent");
 
-module.exports = { register, login, logout, refresh };
+module.exports = { register, login, logout, refresh, getCurrent };
