36f06151184f5086ba41e7ca07db8f98117030c4
Taras <93833612+TarasNetrebko@users.noreply.github.com>
2022-12-10 21:15:25 +0200

    add-remove-transaction

---
+++ controllers/transactions/remove.js
@@ -0,0 +1,13 @@
+const { Transaction } = require("../../models/contacts");
+const ResponseError = require("../../helpers/errorHandler");
+
+const remove = async (req, res) => {
+  const { transactionId } = req.params;
+  const result = await Transaction.findByIdAndDelete(transactionId);
+  if (!result) {
+    throw ResponseError(404, "Transaction Not Found");
+  }
+  res.status(200).json({ message: "transaction deleted" });
+};
+
+module.exports = remove;
