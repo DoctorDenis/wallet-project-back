a7450ad065d6ead5627589b0de7b79ff382ce8c5
DoctorDenis <sychovdenis@icloud.com>
2022-12-11 15:38:15 +0200

    Removed monobank refuse

--- controllers/currency/getCurrency.js
+++ controllers/currency/getCurrency.js
@@ -1,8 +1,12 @@
 const { default: axios } = require("axios");
 
 const getCurrency = async (req, res) => {
-  const data = await axios("https://api.monobank.ua/bank/currency");
-  res.status(200).json(data.data.slice(0, 3));
+  try {
+    const data = await axios("https://api.monobank.ua/bank/currency");
+    console.log(data);
+    res.status(200).json(data.data.slice(0, 3));
+  } catch (error) {
+    res.status(200).send();
+  }
 };
-
 module.exports = getCurrency;
